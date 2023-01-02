993a788b0ff16c0c4013edb18f5e842b14046c86
zodang <1141ekdms@naver.com>
2023-01-02 23:53:05 +0900

    Add boundary

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float boundary = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,29 +14,47 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+        
+        //Add boundary
+        if (transform.position.x <= -boundary)
+        {
+            transform.position = new Vector3(-boundary, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x >= boundary)
+        {
+            transform.position = new Vector3(boundary, transform.position.y, transform.position.z);
+        }
+
+        
+    }
+
+    //Move the player
+    void Move()
+    {
         if (Input.GetKeyDown(KeyCode.UpArrow) | Input.GetKeyDown(KeyCode.DownArrow) | Input.GetKeyDown(KeyCode.LeftArrow) | Input.GetKeyDown(KeyCode.RightArrow))
         {
             //Move forward
             if (Input.GetKeyDown(KeyCode.UpArrow))
             {
-                transform.eulerAngles = new Vector3(0,0,0);
+                transform.eulerAngles = new Vector3(0, 0, 0);
             }
             // Move backward
             if (Input.GetKeyDown(KeyCode.DownArrow))
             {
-                transform.eulerAngles = new Vector3(0,-180,0);
-            }         
+                transform.eulerAngles = new Vector3(0, -180, 0);
+            }
             //Move left   
             if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {                
-                transform.eulerAngles = new Vector3(0,-90,0);
+            {
+                transform.eulerAngles = new Vector3(0, -90, 0);
             }
             //Move right
             if (Input.GetKeyDown(KeyCode.RightArrow))
             {
-                transform.eulerAngles = new Vector3(0,90,0);
+                transform.eulerAngles = new Vector3(0, 90, 0);
             }
-            
+
             transform.Translate(Vector3.forward * 2);
         }
     }
