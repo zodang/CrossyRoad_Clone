ee9c60d32f563041a2eaae832b65e6a883ccc6e2
zodang <1141ekdms@naver.com>
2023-01-02 22:18:44 +0900

    Refactor: 방향키 이용
    방향키로 플레이어 이동시키기

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,43 +4,39 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float rotateSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        Rigidbody playerRb = GetComponent<Rigidbody>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) | Input.GetKeyDown(KeyCode.S) | Input.GetKeyDown(KeyCode.A) | Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.UpArrow) | Input.GetKeyDown(KeyCode.DownArrow) | Input.GetKeyDown(KeyCode.LeftArrow) | Input.GetKeyDown(KeyCode.RightArrow))
         {
             //Move forward
-            if (Input.GetKeyDown(KeyCode.W))
+            if (Input.GetKeyDown(KeyCode.UpArrow))
             {
                 transform.eulerAngles = new Vector3(0,0,0);
             }
             // Move backward
-            if (Input.GetKeyDown(KeyCode.S))
+            if (Input.GetKeyDown(KeyCode.DownArrow))
             {
                 transform.eulerAngles = new Vector3(0,-180,0);
             }         
             //Move left   
-            if (Input.GetKeyDown(KeyCode.A))
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
             {                
                 transform.eulerAngles = new Vector3(0,-90,0);
             }
             //Move right
-            if (Input.GetKeyDown(KeyCode.D))
+            if (Input.GetKeyDown(KeyCode.RightArrow))
             {
                 transform.eulerAngles = new Vector3(0,90,0);
             }
             
             transform.Translate(Vector3.forward * 2);
         }
-
-
     }
 }
