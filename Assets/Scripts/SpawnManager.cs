5a3971280058dd836548ab813c25493dce3f8e0b
zodang <1141ekdms@naver.com>
2023-01-04 19:08:12 +0900

    Feat: 길 생성

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject[] GroundPrefabs;
+    public GameObject[] index;
+    private float posZ = 20;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        int randomIndex = Random.Range(0, GroundPrefabs.Length);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+            if (GroundPrefabs[randomIndex].name == "Ground_Road2")
+            {
+                posZ = posZ + 1;
+            }
+
+            Instantiate(GroundPrefabs[randomIndex], new Vector3(0, 0, posZ), GroundPrefabs[randomIndex].transform.rotation);
+
+            if (GroundPrefabs[randomIndex].name == "Ground_Road2")
+            {
+                posZ = posZ + 1;
+            }
+
+        }
+
         
+
     }
 }
